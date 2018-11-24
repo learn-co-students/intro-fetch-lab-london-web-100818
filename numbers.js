fabc26799336b465c689c136c4f9eefe67961a15
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-24 12:36:57 +0000

    Done.

--- numbers.js
+++ numbers.js
@@ -1,1 +1,82 @@
-// Write your numbers code in this file!
+const fetchTrivia = num =>
+  fetch(`http://numbersapi.com/${num}/trivia`)
+    .then(resp => resp.text())
+
+const showOneTrivia = () => {
+  fetchTrivia(1)
+    .then(trivia => {
+      const oneFacts = document.getElementById('one-facts')
+      oneFacts.innerText = trivia
+    })
+}
+
+const showTrivia = event => {
+  event.preventDefault()
+  const factDiv = document.getElementById('random-math-fact')
+  const numInput = document.getElementById('pick-a-number').value
+
+  factDiv.innerHTML = ''
+  
+  if (isNaN(numInput)) {
+    factDiv.innerText = 'please enter a valid number'
+  } else {
+  fetchTrivia(numInput)
+    .then(trivia => {
+      factDiv.innerText = trivia
+    })
+  }
+}
+
+const fetchYearFact = year =>
+  fetch(`http://numbersapi.com/${year}/year`)
+    .then(resp => resp.text())
+
+const showYearFact = year => {
+  const yearDiv = document.getElementById('year-history')
+
+  yearDiv.innerHTML = ''
+
+  fetchYearFact(year)
+    .then(fact =>{
+      yearDiv.innerText = fact
+    })
+}
+
+const setYearFactInterval = () => {
+  let year = new Date().getFullYear()
+  showYearFact(year)
+  setInterval( () => {
+    --year
+    showYearFact(year)
+  }, 5000)
+}
+
+const fetchAllNumbers = () =>
+  fetch('http://numbersapi.com/1..100')
+    .then(resp => resp.json())
+
+const showAllNumbers = () => {
+  const allNumbersDiv = document.getElementById('all-the-numbers')
+
+  allNumbersDiv.innerHTML = ''
+
+  fetchAllNumbers()
+    .then(allNumbers => {
+      let htmlInsert = '<ul>'
+      for(const number in allNumbers) {
+        htmlInsert += `<li>${allNumbers[number]}</li>`
+      }
+      htmlInsert += '</ul>'
+      allNumbersDiv.innerHTML = htmlInsert
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const numOneBtn = document.getElementById('number-one')
+  numOneBtn.addEventListener('click', showOneTrivia)
+  const triviaInput = document.getElementById('pick-a-number')
+  triviaInput.addEventListener('change', showTrivia)
+  setYearFactInterval()
+  const allNumsBtn = document.getElementById('all-numbers-button')
+  allNumsBtn.addEventListener('click', showAllNumbers)
+})
--- swapi.js
+++ swapi.js
@@ -1,1 +1,59 @@
-// Write your swapi code in this file!
+const fetchSwapi = (type, num) =>
+  fetch(`https://swapi.co/api/${type}/${num}/`)
+    .then(resp => resp.json())
+
+const getOpeningCrawl = () => {
+  fetchSwapi('films', 1)
+    .then(data => {
+      crawlDiv = document.getElementById('crawlDiv')
+      crawlDiv.innerText = data.opening_crawl
+  })
+}
+
+const getPlanet = (event) => {
+  event.preventDefault()
+  const planetInput = parseInt(document.getElementById('planetInput').value)
+  const planetData = document.getElementById('planetData')
+  fetchSwapi('planets', planetInput)
+    .then(data => {
+      planetData.innerHTML =`
+        <br />
+        <p>Planet Name: ${data.name}.</p>
+        <p>Climate: ${data.climate}.</p>
+      `
+    })
+}
+
+const getHomePlanet = (planetUrl, id) => {
+  fetch(planetUrl)
+    .then(resp => resp.json())
+    .then(planet => {
+      document.getElementById(`droid-${id}-homeworld`).innerText = planet.name
+    })
+}
+
+const getDroids = () => {
+  const droidIds = [2, 3]
+  droidIds.map(id => {
+    const droidName = document.getElementById(`droid-${id}-name`)
+    const droidHeight = document.getElementById(`droid-${id}-height`)
+    const droidMass = document.getElementById(`droid-${id}-mass`)
+    const droidBtn = document.getElementById(`droid-${id}-btn`)
+    fetchSwapi('people', id)
+      .then(droid => {
+        droidName.innerText = droid.name
+        droidHeight.innerText = droid.height
+        droidMass.innerText = droid.mass
+        droidBtn.addEventListener('click', () => getHomePlanet(droid.homeworld, id) )
+      })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const crawlBtn = document.getElementById('crawlBtn')
+  crawlBtn.addEventListener('click', getOpeningCrawl)
+  const planetForm = document.getElementById('planetForm')
+  planetForm.addEventListener('submit', getPlanet)
+  const droidsBtn = document.getElementById('find-droids')
+  droidsBtn.addEventListener('click', getDroids)
+})
